cc92eef62bb45f2c06f0c9ee0b48657917a01672
Ali MAKRI <github@makri.fr>
2023-06-19 16:10:13 +0200

    16:10

--- Exo1.sql
+++ Exo1.sql
@@ -1,4 +1,8 @@
--- Créer un base de données Ventes contenant les tables suivantes :
+-- Créer une base de données Ventes contenant les tables suivantes :
 -- Table Produit : id, Libelle, Prix
 -- Table Commande : id, NoCommande, DateCommande
--- Table LigneCommande : id, , Commande, Produit, Quantite, PrixUnitaire
+-- Table LigneCommande : id, Commande, Produit, Quantite, PrixUnitaire
+
+-- 2 commandes pour aujourd'hui
+-- commande 1 : 3 crayons (2.50 l'unité) et 2 stabilo (3.80 l'unité)
+-- commande 2 : 2 cahiers (2.15 l'unité) et 1 agrapheuse (10.60 l'unité)
