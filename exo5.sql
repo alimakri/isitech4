55cbbb39e774f74c7d0c21cd8c2cfa726f588272
Ali MAKRI <github@makri.fr>
2023-06-21 11:23:19 +0200

    11:23

--- exo5.sql
+++ exo5.sql
@@ -1,2 +1,16 @@
 -- Ca pour les 3 catégories
 use VenteBD;
+--select * from Categorie
+--select * from LigneCommande
+--select * from Commande
+--select * from Produit
+
+SELECT 
+	c.Libelle AS cat,
+	ISNULL(SUM(l.Quantite * l.PrixUnitaire), 0) AS ca
+FROM     
+	Produit AS p 
+	RIGHT OUTER JOIN Categorie AS c ON p.Categorie = c.Id 
+	LEFT OUTER JOIN LigneCommande AS l ON p.Id = l.Produit
+GROUP BY c.Libelle
+order by ca desc
